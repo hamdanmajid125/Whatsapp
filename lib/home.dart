1e7acfb114225b06c820d8d14e1fd8065dce955c
hamdanmajid125 <hamdanmajid456@gmail.com>
2021-10-28 23:47:50 +0500

    WhatsappUiDay1

--- lib/home.dart
+++ lib/home.dart
@@ -6,14 +6,31 @@ class WhatsAppHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: HexColor("#23bf52"),
-        title: const Text(
-          "WhatsApp",
-          style: TextStyle(
-            color: Colors.white,
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: const [
+            Icon(Icons.search),
+            SizedBox(width: 30),
+            Icon(Icons.menu),
+            SizedBox(width: 20),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.camera_alt_outlined)),
+              Tab(text: 'CHATS'),
+              Tab(text: 'STATUS'),
+              Tab(text: 'CALLS'),
+            ],
+          ),
+          automaticallyImplyLeading: false,
+          backgroundColor: HexColor("#085f56"),
+          title: const Text(
+            "WhatsApp",
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
